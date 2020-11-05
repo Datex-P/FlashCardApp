937d93b6aaadeae2b627001c26caef0220364ebe
Fabian Weiss <weiss.fabian@outlook.com>
2020-11-05 07:16:32 +0100

    worked on calendar

--- stats.js
+++ stats.js
@@ -2,102 +2,165 @@ import { dataBase } from "./dataBase.js";
 import { redCross as redCrossIcon } from "./svgs.js";
 
 export default function stats() {
+
   let anchorElement = document.querySelector("#questAnswerTrainOverv");
-  anchorElement.style.display = "flex";
+      anchorElement.style.display = "flex";
 
   let mainWindow = document.createElement("div");
-  mainWindow.className = "addQuestionsToDeck";
+      mainWindow.className = "addQuestionsToDeck";
 
   let innerWindow = document.createElement("div");
-  innerWindow.style.marginTop = "20px";
-  innerWindow.style.marginLeft = "30px";
+      innerWindow.style.marginTop = "20px";
+      innerWindow.style.marginLeft = "30px";
 
   let redCrossAndStatsContainer = document.createElement("div");
-  redCrossAndStatsContainer.style.display = "flex";
-  redCrossAndStatsContainer.style.justifyContent = "space-between";
-  redCrossAndStatsContainer.style.height = "20px";
-  redCrossAndStatsContainer.style.width = "255px";
-  redCrossAndStatsContainer.style.border = "1px black solid";
+      redCrossAndStatsContainer.style.display = "flex";
+      redCrossAndStatsContainer.style.justifyContent = "space-between";
+      redCrossAndStatsContainer.style.height = "20px";
+      redCrossAndStatsContainer.style.width = "255px";
+      redCrossAndStatsContainer.style.border = "1px black solid";
 
   let redCross = document.createElement("div");
-  redCross.innerHTML = redCrossIcon;
-  redCross.style.height = "20px";
-  redCross.style.width = "20px";
+      redCross.innerHTML = redCrossIcon;
+      redCross.style.height = "20px";
+      redCross.style.width = "20px";
 
   let theWordStats = document.createElement("div");
-  theWordStats.innerHTML = "Stats";
-  theWordStats.style.fontWeight = "bold";
+      theWordStats.innerHTML = "Stats";
+      theWordStats.style.fontWeight = "bold";
+
+  let todayAndCardsStudiedContainer = document.createElement("div");
+      todayAndCardsStudiedContainer.style.marginTop = "100px";
+      todayAndCardsStudiedContainer.style.display = "flex";
+      todayAndCardsStudiedContainer.style.flexDirection = "column";
+      todayAndCardsStudiedContainer.style.alignItems = "center";
+
+  let cardsStudied = document.createElement("div");
+      cardsStudied.style.display = "flex";
+      cardsStudied.style.justifyContent = "space-between";
+      //cardsStudied.style.border = '1px solid black';
+      cardsStudied.style.width = "300px";
+    
+  let theWordTodayContainer = document.createElement("div");
+      theWordTodayContainer.style.display = "flex";
+      theWordTodayContainer.style.flexDirection = "column";
+      theWordTodayContainer.style.alignItems = "center";
+      theWordTodayContainer.style.marginBottom = "10px";
+
+  let theWordToday = document.createElement("div");
+      theWordToday.innerHTML = "Today";
+      theWordToday.style.fontWeight = "bold";
+      theWordToday.style.marginBottom = '5px';
+
+  let theWordCalendarContainer = document.createElement("div");
+      theWordCalendarContainer.style.display = "flex";
+      theWordCalendarContainer.style.flexDirection = "column";
+
+  let theWordCalendar = document.createElement("div");
+      theWordCalendar.style.marginTop = "10px";
+      theWordCalendar.style.marginBottom = '5px';
+      theWordCalendar.innerHTML = "Calendar";
+      theWordCalendar.style.fontWeight = "bold";
+      
+  let rightAndLeftButtonContainer = document.createElement("div");
+      rightAndLeftButtonContainer.style.display = "flex";
+
+      
+  let buttonLeft = document.createElement("button");
+      buttonLeft.className = "calendarButtons";
+      buttonLeft.style.marginRight = "5px";
+      buttonLeft.innerHTML = "<";
+      
+  let buttonRight = document.createElement("button");
+      buttonRight.className = "calendarButtons";
+      buttonRight.style.marginLeft = "5px";
+      buttonRight.innerHTML = ">";
+  
+      
+  let yearOfStudy = new Date();
+  let year = document.createElement("div");
+      year.innerText = yearOfStudy.getFullYear();
+
+  let dec = 0;
+
+  function decrease() {
+    dec--;
+    year.innerText = yearOfStudy.getFullYear() + dec;
+  }
+
+  function increase() {
+    dec++;
+    year.innerText = yearOfStudy.getFullYear() + dec;
+  }
+
+  buttonLeft.onclick = function () {
+    decrease();
+  };
+
+  buttonRight.onclick = function () {
+    increase();
+  };
+
 
   let container = document.createElement("div");
-  container.style.display = "grid";
+      //container.style.display = "grid";
   //container.style.templateRows =  'repeat(7, 10px)';
   container.style.templateRows = "30px";
   container.style.templateColumns = "20px";
 
   //container.style.templateRows.border = '1px black solid'
 
-  let todayAndCardsStudiedContainer = document.createElement("div");
-  todayAndCardsStudiedContainer.style.marginTop = "100px";
-  todayAndCardsStudiedContainer.style.display = "flex";
-  todayAndCardsStudiedContainer.style.flexDirection = "column";
-  todayAndCardsStudiedContainer.style.alignItems = "center";
+  let yearBoxContainer = document.createElement('div');
+      yearBoxContainer.style.display = 'grid';
+      yearBoxContainer.style.width = '300px';
+      yearBoxContainer.style.height = '210px';
+      yearBoxContainer.style.border = '1px solid black';
+      yearBoxContainer.style.templateRows = '30px';
+      yearBoxContainer.style.templateColumns = '50px';
 
-  let theWordTodayContainer = document.createElement("div");
-  theWordTodayContainer.style.display = "flex";
-  theWordTodayContainer.style.flexDirection = "column";
-  theWordTodayContainer.style.alignItems = "center";
-  theWordTodayContainer.style.marginBottom = "10px";
+  let daysInYearBox = document.createElement('div');
+      daysInYearBox.style.width = '10px';
+      daysInYearBox.style.height = '10px';
+      daysInYearBox.style.border = '1px black solid';
+/*
+  let daysInYearBox2 = document.createElement('div');
+      daysInYearBox2.style.width = '10px';
+      daysInYearBox2.style.height = '10px';
+      daysInYearBox2.style.border = '1px black solid';
+*/
 
-  let theWordToday = document.createElement("div");
-  theWordToday.innerHTML = "Today";
-  theWordToday.style.fontWeight = "bold";
+let thisYear = new Date(`January 1, ${year.innerText}`);
+let nextYear =  new Date(`January 1, ${year.innerText+1}`);
+let daysOfThisYear = ((nextYear-thisYear)/1000/3600/24);
+/*
+for (let i=1; i<=20; i++) {
+  let daysInYearBox${i} = document.createElement('div');
+      daysInYearBox`${i}`.style.width = '10px';
+      daysInYearBox${i}.style.height = '10px';
+      daysInYearBox${i}.style.border = '1px black solid';
+  
+      daysInYearBox.onmouseenter = function () {
+        alert(200, 2019)
+      }
 
-  let theWordCalendarContainer = document.createElement("div");
-  theWordCalendarContainer.style.display = "flex";
-  theWordCalendarContainer.style.flexDirection = "column";
+  yearBoxContainer.append(daysInYearBox)
+      
 
-  let buttonLeft = document.createElement("button");
-  buttonLeft.className = "calendarButtons";
-  buttonLeft.style.marginRight = "5px";
-  buttonLeft.innerHTML = "<";
-  let dec = 0;
-  let yearOfStudy = new Date();
-  function decrease() {
-    dec--;
-    year.innerHTML = yearOfStudy.getFullYear() + dec;
-  }
 
-  function increase() {
-    dec++;
-    year.innerHTML = yearOfStudy.getFullYear() + dec;
-  }
 
-  buttonLeft.onclick = function () {
-    decrease();
-    /*let dec = 1;
-        console.log(typeof((yearOfStudy.getFullYear()+dec).toString()))
-        */
-  };
+      yearBoxContainer.append(daysInYearBox2)
 
-  let buttonRight = document.createElement("button");
-  buttonRight.className = "calendarButtons";
-  buttonRight.style.marginLeft = "5px";
-  buttonRight.innerHTML = ">";
 
-  buttonRight.onclick = function () {
-    increase();
-  };
 
-  let rightAndLeftButtonContainer = document.createElement("div");
-  rightAndLeftButtonContainer.style.display = "flex";
+}
 
-  let year = document.createElement("div");
-  year.innerHTML = yearOfStudy.getFullYear();
+*/
 
-  let theWordCalendar = document.createElement("div");
-  theWordCalendar.style.marginTop = "10px";
-  theWordCalendar.innerHTML = "Calendar";
-  theWordCalendar.style.fontWeight = "bold";
+
+console.log(daysOfThisYear);
+
+
+
 
   /*
   seconds studied in total
@@ -106,12 +169,6 @@ export default function stats() {
     let secStudied = document.getElementById("secondsStudied");
     secStudied.innerHTML = counterSecStudied.reduce((acc, cur) => acc + cur);
   */
-
-  let cardsStudied = document.createElement("div");
-  cardsStudied.style.display = "flex";
-  cardsStudied.style.justifyContent = "space-between";
-  //cardsStudied.style.border = '1px solid black';
-  cardsStudied.style.width = "300px";
 
   for (let deck in dataBase.counter) {
     cardsStudied.innerHTML += `Deck ${deck}: ${dataBase.counter[deck]} cards studied<br/>`;
@@ -122,17 +179,9 @@ export default function stats() {
 
   // console.log(dataBase.DeckNames.Literature.cardsStudied)
   if (cardsStudied.innerHTML === "") {
-    cardsStudied.innerHTML = "No cards studied today";
+      cardsStudied.innerHTML = "No cards studied today";
   }
-
-  let wordToday = document.createElement("div");
-  wordToday.innerHTML = "Today";
-  wordToday.fontWeight = "bold";
-
-  let cardCounts = document.createElement("div");
-  cardCounts.innerHTML = "Card Counts";
-  cardCounts.fontWeight = "bold";
-
+ 
   /*
   let arr = Object.keys(obj);
   let numberOfSeconds.innerHTML = arr.forEach((item) => {
@@ -144,14 +193,16 @@ export default function stats() {
   theWordTodayContainer.appendChild(cardsStudied);
 
   todayAndCardsStudiedContainer.appendChild(theWordTodayContainer);
-
   todayAndCardsStudiedContainer.appendChild(theWordTodayContainer);
 
   theWordCalendarContainer.appendChild(theWordCalendar);
   rightAndLeftButtonContainer.appendChild(buttonLeft);
   rightAndLeftButtonContainer.appendChild(year);
   rightAndLeftButtonContainer.appendChild(buttonRight);
+
   theWordCalendarContainer.append(rightAndLeftButtonContainer);
+  yearBoxContainer.append(daysInYearBox)
+  theWordCalendarContainer.append(yearBoxContainer)
 
   todayAndCardsStudiedContainer.appendChild(theWordCalendarContainer);
   redCrossAndStatsContainer.appendChild(theWordStats);
@@ -162,6 +213,8 @@ export default function stats() {
 
   mainWindow.append(innerWindow);
   anchorElement.appendChild(mainWindow);
+
+
 
   setTimeout(function () {
     window.onclick = function handleOutsideClick(e) {
