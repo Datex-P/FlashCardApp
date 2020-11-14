9fd4c0631a71fce220e48b698d2febf8eb41e2a8
Валерій Зінченко <mac@MacBook-Air-Valerij.local>
2020-11-14 15:35:05 +0200

    fixing error

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import createDom from './createDom.js';
-import {dataBase} from './dataBase.js';
+import { dataBase } from './dataBase.js';
 import stats from './stats.js';
 import settings from './settings.js';
 import createNewDeck from './createNewDeck.js'
@@ -10,40 +10,48 @@ import createNewDeck from './createNewDeck.js'
 
 createDom(dataBase.DeckNames);
 
-
 let opened = false;
-document.querySelector('.menu').onclick = function () {
+function handleOutsideClick(e) {
+  if ( !(document.querySelector('.menuBox').contains(e.target)) ) {
+    //alert("Clicked outside Box");
+    document.querySelector('.menuBox').style.display = 'none';
+    opened = false;
+    console.log('window handler still alive')
+    window.onclick = ''
+  }
   
-   if (!opened)
-   {
-  document.querySelector('.menuBox').style.display = 'flex';
-  document.querySelector('.menuBox').style.justifyContent = 'space-around';
-  //opened = true;
-   }
-  //  if (opened) {
-  //    document.querySelector('.menuBox').style.display = 'none';
-  //    opened = false;
-  //  }
-    setTimeout(function () {
-    window.onclick = function handleOutsideClick(e) {
-      if (document.querySelector('.menuBox').contains(e.target)) {
-        //alert("Clicked in Box");
-      } else {
-        //alert("Clicked outside Box");
-      document.querySelector('.menuBox').style.display = 'none';
-     opened = false;
-      }
-    } 
-  }, 10);
+}
+
+
+document.querySelector('.menu').onclick = function () {
+
+  if (!opened) {
+    document.querySelector('.menuBox').style.display = 'flex';
+    document.querySelector('.menuBox').style.justifyContent = 'space-around';
+    opened = true;
+    setTimeout(()=>{
+      window.onclick = handleOutsideClick;
+    },10)
+  } else {
+    document.querySelector('.menuBox').style.display = 'none';
+    opened = false;
+    window.onclick = ''
+  }
+
 };
 
-let boxesInMenu = document.getElementsByClassName('menuBoxesStyling');
 
-  for (let i=0; i<boxesInMenu.length; i++) {
-    boxesInMenu[i].onmouseover = function (e) {
-      e.target.backgroundColor = 'blue';
-    }
+let boxesInMenu = document.querySelectorAll('.menuBoxesStyling');
+boxesInMenu.forEach(button=>{
+  button.onclick = function(){
+    alert(button.innerText)
   }
+})
+// for (let i=0; i<boxesInMenu.length; i++) {
+//   boxesInMenu[i].onmouseover = function (e) {
+//     e.target.style.backgroundColor = 'blue';
+//   }
+// }
 
 
 
@@ -57,9 +65,9 @@ let boxesInMenu = document.getElementsByClassName('menuBoxesStyling');
 
 document.getElementById("createDeckButton").onclick = function () {
   createNewDeck()
- 
+
   document.querySelector(".arrowDown").style.display = "none";
-  
+
 };
 
 
