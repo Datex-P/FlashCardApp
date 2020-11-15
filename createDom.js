2f36300bc9d4307be5c524f30be5c715493a8c99
Fabian Weiss <weiss.fabian@outlook.com>
2020-11-15 12:38:29 +0100

    small changes

--- createDom.js
+++ createDom.js
@@ -3,6 +3,25 @@ import questAnswerTrainOverv from './questAnswerTrainOverv.js';
 import addQuestionsToDeck from './addQuestionsToDeck.js';
 import { dataBase } from './dataBase.js';
 
+
+function createElement(tag = 'div', inner = '', style = {}, className = null, id = null) {
+
+  let element = document.createElement(tag);
+
+  element.innerHTML = inner;
+  if (id) {
+    element.id = id;
+  }
+  if (className) {
+    element.className = className;
+  }
+  for (let prop in style) {
+    element.style[prop] = style[prop]
+  }
+  return element
+}
+
+
 export default function createDom(obj, length = "long") {
 
   listOfDecks.innerHTML = "";
@@ -12,29 +31,10 @@ export default function createDom(obj, length = "long") {
 
 
 
-  listOfDecks.onwheel = function () {
-    // z Index of visible card  (currently 100) is 1
-      //z Index of all the other cards get incremented by 1
-      //how to get card with hightest zIndex
-
-      //for x in listofDecks
-     
-  //     function maxZIndex() {
-
-  //       console.log(Array.from(document.querySelectorAll('newDeckContainer'))
-  //             .map(a => parseFloat(window.getComputedStyle(a).zIndex))
-  //             .filter(a => !isNaN(a))
-  //             .sort()
-  //             .pop()
-  //  }
-  //  maxZIndex()
-
-  }
 
 
 
-
-  let colors = ['red','blue','green','yellow','orange']
+  let colors = ['#ffcdb2','#ffb4a2','#e5989b','#b5838d','#6d6875']
   
   arr.forEach((item, index) => {
     let newDeckContainer = document.createElement("div");
@@ -52,7 +52,8 @@ export default function createDom(obj, length = "long") {
     }
 
     let nameOfNewDeck = document.createElement("div");
-    nameOfNewDeck.innerText = item;
+        nameOfNewDeck.innerText = item;
+        //nameOfNewDeck.style.fontWeight = 'bold';
 
     nameOfNewDeck.onmouseover = function () {
       nameOfNewDeck.style.color = "rgb(200, 168, 115)";
@@ -79,23 +80,43 @@ export default function createDom(obj, length = "long") {
 
     let addEditDeleteContainer = document.createElement("div");
     addEditDeleteContainer.style.display = "flex";
+    addEditDeleteContainer.style.flexDirection = 'column';
     addEditDeleteContainer.style.justifyContent = "space-around";
-    addEditDeleteContainer.style.width = "100px";
-    addEditDeleteContainer.style.marginTop = '60px'
-    addEditDeleteContainer.style.alignItems = "center";
+    addEditDeleteContainer.style.width = "140px";
+    addEditDeleteContainer.style.height = '120px';
+    addEditDeleteContainer.style.marginTop = '20px'
+    //addEditDeleteContainer.style.alignItems = "center";
+    addEditDeleteContainer.style.left = '105px';
+    addEditDeleteContainer.style.border = '1px black solid'
     addEditDeleteContainer.style.zIndex = '3';
     addEditDeleteContainer.style.position = 'absolute';
 
 
 
+   // ['', '', '', '', '', '', '', ''].forEach()
+
+
+
+
+    // ['a', 'a', 'a'].forEach(el => {
+    //   let blackLines12 = document.createElement('div');
+    //   blackLines12.style.width = '99%';
+    //   blackLines12.style.border= '0.5px #eee4e1 solid'
+    //   blackLines12.innerText = el
+    //   //container2min.append(smallerThan);
+
+    //   newDeckContainer.append(blackLines12)
+    // });
+
+
     let blackLines = document.createElement('div');
         blackLines.style.width = '99%';
-        blackLines.style.border= '0.5px #f7ede2 solid'
+        blackLines.style.border= '0.5px #eee4e1 solid'
 
     let blackLines2 = document.createElement('div');
         blackLines2.style.marginTop = '15px';
         blackLines2.style.width = '99%';
-        blackLines2.style.border= '0.5px #ffe8d6 solid'
+        blackLines2.style.border= '0.5px #eee4e1 solid'
 
     let blackLines3 = document.createElement('div');
         blackLines3.style.marginTop = '15px';
@@ -105,17 +126,41 @@ export default function createDom(obj, length = "long") {
     let blackLines4 = document.createElement('div');
         blackLines4.style.marginTop = '15px';
         blackLines4.style.width = '99%';
-        blackLines4.style.border= '0.5px #ffe1a8 solid';
+        blackLines4.style.border= '0.5px #eee4e1 solid';
 
     let blackLines5 = document.createElement('div');
         blackLines5.style.marginTop = '15px';
         blackLines5.style.width = '99%';
-        blackLines5.style.border= '0.5px #ffeedd solid';
+        blackLines5.style.border= '0.5px #eee4e1 solid';
+
+    let blackLines6 = document.createElement('div');
+        blackLines6.style.marginTop = '15px';
+        blackLines6.style.width = '99%';
+        blackLines6.style.border= '0.5px #eee4e1 solid';
+
+    let blackLines7 = document.createElement('div');
+        blackLines7.style.marginTop = '15px';
+        blackLines7.style.width = '99%';
+        blackLines7.style.border= '0.5px #eee4e1 solid';
+
+    let blackLines8 = document.createElement('div');
+        blackLines8.style.marginTop = '15px';
+        blackLines8.style.width = '99%';
+        blackLines8.style.border= '0.5px #eee4e1 solid';
 
     let trashIcon = document.createElement("div");
     trashIcon.innerHTML = trash;
     trashIcon.style.right = "5px";
-    trashIcon.querySelector('svg').style.color = 'red';
+    //trashIcon.querySelector('svg').style.color = 'red';
+
+
+    let trashIconContainer = document.createElement('div');
+        trashIconContainer.style.border = '1px black solid';
+        trashIconContainer.style.display = 'flex';
+        trashIconContainer.style.justifyContent = 'space-around';
+
+    let trashIconText = document.createElement('div');
+        trashIconText.innerHTML = 'Delete deck';
 
 
     trashIcon.onclick = () => {
@@ -130,12 +175,28 @@ export default function createDom(obj, length = "long") {
     };
 
     let editIcon = document.createElement("div");
-    editIcon.style.marginRight = "5px";
+    //editIcon.style.marginRight = "5px";
+    editIcon.style.left = '190px'
+    editIcon.style.position = 'absolute';
+    editIcon.style.top = '11px'
+
+
+     let addIcon = document.createElement("div");
+    //addIcon.style.marginRight = "5px";
+    // addIcon.style.left = '180px'
+    // addIcon.style.position = 'absolute';
+    // addIcon.style.top = '11px'
+
+
+
+
+
+
 
 
     let edited = false;
     editIcon.innerHTML = edit;
-    editIcon.querySelector('svg').style.color = 'orange';
+    //editIcon.querySelector('svg').style.color = 'orange';
 
     let changeNameofDeckInput = document.createElement("input");
     changeNameofDeckInput.onclick = function(event){
@@ -167,7 +228,7 @@ export default function createDom(obj, length = "long") {
        // console.log('click like a edit')
       } else {
         this.innerHTML = edit; 
-        editIcon.querySelector('svg').style.color = 'orange';
+       // editIcon.querySelector('svg').style.color = 'orange';
         //console.log('click like a save')
         newDeckContainer.replaceChild(nameOfNewDeck, changeNameofDeckInput);
         window.removeEventListener('click',clickOutsideHandle)
@@ -185,31 +246,99 @@ export default function createDom(obj, length = "long") {
     el.style.cursor = 'pointer';
   });
 
+  let addIconContainer = document.createElement('div');
+      //addIconContainer.style.border = '1px black solid';
+      //addIconContainer.style.display = 'flex';
+     // addIconContainer.style.transform = 'rotate(180deg)';
+      addIconContainer.style.left = '190px'
+      addIconContainer.style.position = 'absolute';
+      addIconContainer.style.top = '11px';
+      addIconContainer.style.cursor = 'pointer';
+
+    // transform: 'rotate(90deg)',
+    // fontWeight: 'bold',
+    // marginTop:'5px',
+    // display: 'none',
+    // position: 'absolute',
 
 
 
-
-    let addIcon = document.createElement("div");
-    addIcon.className = 'orangeCircle';
-    addIcon.innerText = "+";
-    addIcon.style.border = '1px solid red';
+    
 
 
-    addIcon.onclick = function () {
-      this.classList.remove('blinkingIcon')
-      addQuestionsToDeck(item);
+    let littleModalWindow = createElement(
+      'div',
+      '',
+      {
+        transform: 'rotate(-90deg)'
+      },
+      'littleModalWindow2'
+    )
+
+
+
+    addIconContainer.onclick = function () {
+
+      littleModalWindow.style.display = 'block'
     }
 
-    addIcon.addEventListener('mouseover', function() {
-      addIcon.style.cursor = 'pointer';
-    });
+
+
+
+
+
+
+
+
+
+    
+
+   // let addIcon = document.createElement("div");
+    // addIcon.className = 'orangeCircle';
+    // addIcon.innerText = "+";
+    // addIcon.style.color = 'white'
+    // addIcon.style.border = '1px solid grey';
+
+    //addIcon.style.zIndex = '3';
+    addIcon.style.transform = 'rotate(90deg)';
+    addIcon.innerText='...';
+    addIcon.style.fontWeight = 'bold';
+  
+
+    // addIcon.onclick = function () {
+    //   this.classList.remove('blinkingIcon')
+    //   addQuestionsToDeck(item);
+    // }
+
+    // addIcon.addEventListener('mouseover', function() {
+    //   addIcon.style.cursor = 'pointer';
+    // });
 
 
     newDeckContainer.appendChild(nameOfNewDeck);
+
+    //newDeckContainer.append(editIcon)
+    newDeckContainer.append(addIconContainer)
+
+
+    addIconContainer.append(littleModalWindow)
+    addIconContainer.append(addIcon)
+
+
     if (length == "long") {
-      addEditDeleteContainer.appendChild(addIcon);
-      addEditDeleteContainer.appendChild(editIcon);
-      addEditDeleteContainer.appendChild(trashIcon);
+     // addEditDeleteContainer.appendChild(addIcon);
+      //addEditDeleteContainer.appendChild(editIcon);
+
+      //addEditDeleteContainer.append(addIconContainer);
+     // addIconContainer.append(addIcon);
+     // addIconContainer.append(addIconText)
+
+
+      addEditDeleteContainer.appendChild(trashIconContainer)
+
+      trashIconContainer.append(trashIcon)
+      trashIconContainer.append(trashIconText)
+      //addEditDeleteContainer.appendChild(trashIcon);
 
 
       newDeckContainer.appendChild(blackLines);
@@ -217,7 +346,9 @@ export default function createDom(obj, length = "long") {
       newDeckContainer.appendChild(blackLines3);
       newDeckContainer.appendChild(blackLines4);
       newDeckContainer.appendChild(blackLines5);
-
+      newDeckContainer.appendChild(blackLines6);
+      newDeckContainer.appendChild(blackLines7);
+      newDeckContainer.appendChild(blackLines8);
     }
 
     newDeckContainer.appendChild(addEditDeleteContainer);
