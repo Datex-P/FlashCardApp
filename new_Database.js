cb9dc193cf135e5164e936312ef9f88abf7002d9
Валерій Зінченко <mac@MacBook-Air-Valerij.local>
2020-12-12 12:13:25 +0200

    new structure

---
+++ new_Database.js
@@ -0,0 +1,32 @@
+
+let base = {
+  decks: [
+    {
+      to_stydy: Number,
+      to_review: Number,
+      cards: [
+        {
+          question: String,
+          answer: String
+        },
+        ...
+      ]
+    },
+    ...
+  ],
+  history:{
+    '12.12.2020': [
+      {
+        deck: String,
+        cardIndex: Number,
+        spendTime: Number,
+        timestamp: Date
+      }
+    ]
+  },
+  studySmartGoal: {
+    amount: 100,
+    topic: String,
+    deadLine: Date
+  }
+}
